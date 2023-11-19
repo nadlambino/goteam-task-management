2d31a66ae03e806042d5885dfb7e493e310e3d35
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-11-19 16:03:38 +0800

    ref: create task service and move task business logic from controller to service

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
-use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
+use App\Services\TaskService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 
 class TaskController extends Controller
 {
+    public function __construct(protected TaskService $taskService) { }
+
     /**
      * Get tasks of authenticated user
      *
@@ -24,36 +24,7 @@ class TaskController extends Controller
         $search = $request->get('search');
         $status = $request->get('status');
         $due = $request->get('due');
-
-        $tasks = Auth()
-            ->user()
-            ->tasks()
-            ->when(!empty($search), function($query) use ($search) {
-                $query->where(function($query) use ($search) {
-                    $query->where('title', 'LIKE', "%$search%")
-                        ->orWhere('description', 'LIKE', "%$search%");
-                });
-            })
-            ->when(!empty($status), function($query) use ($status) {
-                $query->where('status', $status);
-            })
-            ->when(!empty($due), function($query) use ($due) {
-                if ($due === 'today') {
-                    return $query->whereBetween('due_at', [
-                        Carbon::today()->startOfDay(),
-                        Carbon::today()->endOfDay()
-                    ])
-                    ->where('status', '!=', 'Done');
-                }
-                if ($due === 'past') {
-                    return $query->where('due_at', '<', Carbon::today())
-                    ->where('status', '!=', 'Done');
-                }
-            })
-            ->orderBy('sort')
-            ->orderByDesc('updated_at')
-            ->get()
-            ->toArray();
+        $tasks = $this->taskService->getTasks($search, $status, $due);
 
         return $this->successResponse($tasks);
     }
@@ -67,8 +38,7 @@ class TaskController extends Controller
     public function store(StoreTaskRequest $request): JsonResponse
     {
         $data = $request->validated();
-        $data['due_at'] = Carbon::parse($data['due_at'])->format('Y-m-d H:i:s');
-        $task = Auth()->user()->tasks()->create($data);
+        $task = $this->taskService->createTask($data);
 
         if ($task) {
             return $this->successResponse($task->toArray(), code: 201);
@@ -85,10 +55,7 @@ class TaskController extends Controller
      */
     public function show(string $id): JsonResponse
     {
-        $task = Auth()
-            ->user()
-            ->tasks()
-            ->find($id);
+        $task = $this->taskService->getTaskById($id);
 
         if (!$task) {
             return $this->errorResponse("Task with id of $id does not exists", code: 404);
@@ -107,16 +74,7 @@ class TaskController extends Controller
     public function update(UpdateTaskRequest $request, Task $task): JsonResponse
     {
         $data = $request->validated();
-        $data['due_at'] = Carbon::parse($data['due_at'])->format('Y-m-d H:i:s');
-
-        $data['started_at'] = match (true) {
-            !isset($data['started_at']) && $data['status'] !== 'Todo'   => now(),
-            $data['status'] === 'Todo'                                  => null,
-            !isset($data['started_at'])                                 => now(),
-            default                                                     => null
-        };
-        
-        $task->update($data);
+        $task = $this->taskService->updateTask($task, $data);
 
         return $this->successResponse($task->toArray());
     }
@@ -129,13 +87,11 @@ class TaskController extends Controller
      */
     public function destroy(string $id): JsonResponse
     {
-        $task = Auth::user()->tasks()->find($id);
+        $task = $this->taskService->deleteTask($id);
 
         if (!$task) {
             return $this->errorResponse("Task with id of $id does not exists", code: 404);
         }
-
-        $task->delete();
 
         return $this->successResponse();
     }
---
+++ app/Services/TaskService.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Task;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class TaskService 
+{
+    public function __construct(protected Task $task) { }
+
+    /**
+     * Get tasks of authenticated user
+     *
+     * @param string|null $search
+     * @param string $status
+     * @param string|null $due
+     * @return array
+     */
+    public function getTasks(?string $search, ?string $status, ?string $due): array
+    {
+        return Auth()
+            ->user()
+            ->tasks()
+            ->when(!empty($search), function($query) use ($search) {
+                $query->where(function($query) use ($search) {
+                    $query->where('title', 'LIKE', "%$search%")
+                        ->orWhere('description', 'LIKE', "%$search%");
+                });
+            })
+            ->when(!empty($status), function($query) use ($status) {
+                $query->where('status', $status);
+            })
+            ->when(!empty($due), function($query) use ($due) {
+                if ($due === 'today') {
+                    return $query->whereBetween('due_at', [
+                        Carbon::today()->startOfDay(),
+                        Carbon::today()->endOfDay()
+                    ])
+                    ->where('status', '!=', 'Done');
+                }
+                if ($due === 'past') {
+                    return $query->where('due_at', '<', Carbon::today())
+                    ->where('status', '!=', 'Done');
+                }
+            })
+            ->orderBy('sort')
+            ->orderByDesc('updated_at')
+            ->get()
+            ->toArray();
+    }
+
+    /**
+     * Create a task
+     *
+     * @param array $data
+     * @return Task|null
+     */
+    public function createTask(array $data): ?Task
+    {
+        $data['due_at'] = $this->parseDate($data['due_at']);
+
+        return Auth()->user()->tasks()->create($data);
+    }
+
+    /**
+     * Get task by ID
+     *
+     * @param string|integer $id
+     * @return Task|null
+     */
+    public function getTaskById(string|int $id): ?Task
+    {
+        return Auth()
+            ->user()
+            ->tasks()
+            ->find($id);
+    }
+
+    /**
+     * Update a task
+     *
+     * @param Task $task
+     * @param array $data
+     * @return Task
+     */
+    public function updateTask(Task $task, array $data): Task
+    {
+        $data['due_at'] = $this->parseDate($data['due_at']);
+
+        $data['started_at'] = match (true) {
+            !isset($data['started_at']) && $data['status'] !== 'Todo'   => now(),
+            $data['status'] === 'Todo'                                  => null,
+            !isset($data['started_at'])                                 => now(),
+            default                                                     => null
+        };
+        
+        $task->update($data);
+
+        return $task;
+    }
+
+    /**
+     * Delete a task
+     *
+     * @param string|integer $id
+     * @return boolean
+     */
+    public function deleteTask(string|int $id): bool
+    {
+        $task = Auth::user()->tasks()->find($id);
+
+        if (!$task) {
+            return false;
+        }
+
+        return $task->delete();
+    }
+
+    /**
+     * Parse a date and convert to proper format
+     *
+     * @param string $date
+     * @return string
+     */
+    private function parseDate(string $date): string
+    {
+        return Carbon::parse($date)->format('Y-m-d H:i:s');
+    }
+}
