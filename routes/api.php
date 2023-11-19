c9614f30bbb02eb0a61ced53673b8ac8fd9ee47b
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-11-19 09:26:38 +0800

    ref: code structure

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', fn() => Auth::user());
     Route::apiResource('tasks', TaskController::class);
 });
-
