b0cb95ff5e0a30e519c8355eb39ba3107e55b137
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-11-19 11:24:38 +0800

    fix: style issue when asset build

--- resources/js/components/RouterLinkButton.vue
+++ resources/js/components/RouterLinkButton.vue
@@ -14,6 +14,6 @@ defineProps<{
 <style scoped lang="scss">
 .btn-link {
     @apply px-2 py-1 no-underline rounded-md bg-blue-500 border-2 border-blue-500 border-solid 
-    text-white capitalize tracking-wide hover:bg-blue-400 hover:border-blue-400;
+    text-white capitalize tracking-wide hover:bg-blue-400 hover:border-blue-400 hover:text-white;
 }
 </style>
