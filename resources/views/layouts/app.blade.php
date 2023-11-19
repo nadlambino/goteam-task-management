bc028854d6cb39d4c6b53cff1a398ca16a596fe9
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-11-19 17:48:00 +0800

    ref: change brand name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'GoTeam') }}</title>
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.ts'])
