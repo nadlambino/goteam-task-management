ba901a9e33d57d31742501b3cb6c0c26e0d118a9
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-11-19 12:13:21 +0800

    feat: add axios interceptor to reload page for 401 error

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,5 +1,5 @@
 import 'bootstrap';
-import axios, { type AxiosStatic } from 'axios';
+import axios, { AxiosError, type AxiosStatic } from 'axios';
 
 declare global {
   interface Window {
@@ -10,4 +10,12 @@ declare global {
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.withCredentials = true
+window.axios.defaults.withCredentials = true;
+window.axios.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+        if (error.response?.status === 401) {
+            window.location.reload();
+        }
+    }
+);
